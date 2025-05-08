619aff773bcf27d74330f3f283b08f7cb4abf24e
vsoto06 <vsoto65@student.ccc.edu>
2025-05-07 19:21:29 -0500

    Add files via upload
    p15 soto

---
+++ p15.py
@@ -0,0 +1,72 @@
+
+class Employee:
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@email.com'
+        self.pay = pay
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+    @classmethod
+    def set_raise_amt(cls,amount):
+        cls.raise_amt=amount
+
+    @classmethod
+    def from_string(cls,emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday()== 5 or day.weekday() == 6:
+            return False
+        return True
+
+emp_1 = Employee('corey','schafer',50000)
+emp_2 = Employee('test','user',60000)
+    
+class Developer(Employee):
+    raise_amt = 1.10
+
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+
+class Manager(Employee):
+
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+
+dev_1 = Developer('corey', 'schafer', 50000, 'Python')
+dev_2 = Developer('test', 'Employee', 60000, 'Java')
+
+mgr_1 = Manager('sue', 'smith', 90000, [dev_1])
+
+print(issubclass(Manager, Employee))
+mgr_1.add_emp(dev_2)
+mgr_1.print_emps()
