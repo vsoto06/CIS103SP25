4043f7b7dd14b6331047df1c55388cb551720dd8
vsoto06 <vsoto65@student.ccc.edu>
2025-05-07 19:10:58 -0500

    Add files via upload
    p2-victor soto

---
+++ p2.py
@@ -0,0 +1,12 @@
+pie = 3.1459
+aa = int(input('Enter your width: '))
+bb = int(input('Enter your height for rect: '))
+zz = aa * bb
+print('aa * bb is: ', zz)
+cc = int(input('Enter your base for your tri'))
+dd = int(input('Enter your Height for your tri'))
+xx = cc * dd / 2
+print('cc*dd/2', xx)
+ee = int(input('Enter your radius'))
+yy = pie * ee**2
+print('pie*ee**2',yy)
